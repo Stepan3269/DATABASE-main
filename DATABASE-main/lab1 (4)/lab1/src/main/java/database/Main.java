50cf05afc168dc943a4fd653a9a294b5e9897e57
Stepan3269 <121955652+Stepan3269@users.noreply.github.com>
2023-01-16 09:33:04 +0200

    Update Main.java

--- DATABASE-main/lab1 (4)/lab1/src/main/java/database/Main.java
+++ DATABASE-main/lab1 (4)/lab1/src/main/java/database/Main.java
@@ -2,7 +2,7 @@ package database;
 
 public class Main {
     public static void main(String[] args) {
-        DBManager dbManager = new DBManager("jdbc:mysql://localhost:3306/hospital", "vitalii", "02032003v");
+        DBManager dbManager = new DBManager("jdbc:mysql://localhost:3306/hospital", "stepan", "02032003v");
         HospitalService hospitalService = new HospitalService(dbManager);
 
         hospitalService.createTables();
